ff5a7b43eca5d01870de172b021c2287e7467434
dzidzoiev <artur.dzidzoiev@gmail.com>
2017-03-27 22:36:16 +0200

    Fixed cors for dev.  Added script to start docker container. Closes #10

--- servercode/src/mongo.js
+++ servercode/src/mongo.js
@@ -4,6 +4,9 @@ const config = require("./config/config");
 exports.connect = () => {
     mongoose.Promise = global.Promise;
     return mongoose.connect('mongodb://' + config.mongo.host + ':' + config.mongo.port + '/exampleDb')
-        .then(console.info('Connected to Mongo.'))
-        .catch((err) => console.error('Error connecting to Mongo' + err));
+        .then(() => console.info('Connected to Mongo.'))
+        .catch((err) => {
+            console.error('Error connecting to Mongo' + err);
+            reject();
+        })
 };
