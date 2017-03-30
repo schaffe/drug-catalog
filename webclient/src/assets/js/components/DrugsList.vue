47daf202507a7d68a10e47fe1c36b5660c3e7d19
Yuliia Dzidzoieva <kastrulya@users.noreply.github.com>
2017-03-31 00:05:00 +0300

    [FE] Add new drug (#16)

--- webclient/src/assets/js/components/DrugsList.vue
+++ webclient/src/assets/js/components/DrugsList.vue
@@ -1,6 +1,25 @@
 <template>
     <div class="drugsList">
         <h2>Drugs in your home</h2>
+        <h3>Have you something new?</h3>
+        <div class="field">
+            <label for="title">Title</label>
+            <input type="title" id="title" v-model="title">
+        </div>
+        <div class="field">
+            <label for="category">Category</label>
+            <input type="title" id="category" v-model="category">
+        </div>
+        <div class="field">
+            <label for="due_date">Due date</label>
+            <input type="date" id="due_date" v-model="due_date">
+        </div>
+        <div class="field">
+            <label for="amount">Amount</label>
+            <input type="number" id="amount" v-model="amount">
+        </div>
+        <input type="submit" value="Add" @click="addDrug">
+
         <table class="list">
             <tr>
                 <th>Title</th>
@@ -29,12 +48,26 @@ export default{
     },
     data(){
         return {
+            title: '',
+            category: '',
+            due_date: new Date(),
+            amount: 0
         };
     },
     computed: {
         drugs() {
             return store.getters.drugsList;
         }
+    },
+    methods: {
+        addDrug(){
+            store.dispatch('addDrug',{
+                title: this.title,
+                group: this.category.split(' '),
+                bestBefore: this.due_date,
+                number: this.amount
+            })
+        },
     }
 }
 </script>
@@ -44,6 +77,15 @@ export default{
         color: cadetblue;
     }
 
+    .field {
+        display: inline-block;
+    }
+
+
+    .field>input {
+        display: block;
+    }
+    
     .list {
         width: 100%;
         border-spacing: 15px;
