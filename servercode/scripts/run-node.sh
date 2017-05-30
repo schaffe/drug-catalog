2442b356cf69bb4ba1d307d05d6bae632744e395
dzidzoiev <artur.dzidzoiev@gmail.com>
2017-05-30 21:11:52 +0200

    #4 Removed consul from dependencies, fixed travis buid scripts

--- servercode/scripts/run-node.sh
+++ servercode/scripts/run-node.sh
@@ -3,7 +3,12 @@
 echo ${ENV};
 if [ "${ENV}" == 'DEV' ]; then
     #todo not killing normally
-    nodemon src/index.js
+    nodemon src/index.js;
+    exit 0;
+fi
+
+if [ "${ENV}" == 'TEST' ]; then
+    npm test;
     exit 0;
 fi
 
