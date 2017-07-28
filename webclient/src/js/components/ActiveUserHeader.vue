f8e6afbccc568bb05ca2d902ab0f7da771d2a3de
dzidzoiev <artur.dzidzoiev@gmail.com>
2017-07-28 23:25:38 +0200

    #29 nice toolbar

--- webclient/src/js/components/ActiveUserHeader.vue
+++ webclient/src/js/components/ActiveUserHeader.vue
@@ -1,10 +1,28 @@
 <template>
     <div>
         <router-link v-if="!me" class="login" to="login">Login</router-link>
-        <div v-else>
-            <span>{{me.name}}</span>
-            <button @click="logout()">Logout</button>
-        </div>
+        <v-toolbar v-else flat class="transparent">
+            <v-list class="pa-0 transparent">
+                <v-list-tile avatar tag="div">
+                    <v-list-tile-avatar>
+                        <img src="https://randomuser.me/api/portraits/men/86.jpg"/>
+                    </v-list-tile-avatar>
+                    <v-list-tile-content>
+                        <v-list-tile-title class="white--text">{{me.name}}</v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+            </v-list>
+            <v-spacer></v-spacer>
+            <v-btn icon>
+                <v-icon>search</v-icon>
+            </v-btn>
+            <v-btn icon>
+                <v-icon>refresh</v-icon>
+            </v-btn>
+            <v-btn icon @click="logout">
+                <v-icon>fingerprint</v-icon>
+            </v-btn>
+        </v-toolbar>
     </div>
 </template>
 
@@ -18,8 +36,7 @@
             store.dispatch('getUser');
         },
         data(){
-            return {
-            };
+            return {};
         },
         computed: {
             me() {
